85c12f51409d9a612aa1d6e1b758d4de2477f59b
J.kim <jaeseok.kim@griffithuni.edu.au>
2023-06-11 22:49:24 +1000

    Search bar & Search func

--- src/index.js
+++ src/index.js
@@ -9,10 +9,17 @@ function setUpperCase(string="No description")
 }
 const perPageNum = 7
 var pNum = 1
+var throttle = true
 const callback = (entries,observer)=>{
     entries.forEach(entry => {
-        if (entry.isIntersecting){
-            random_fetchAPI(pNum,28)
+        if (entry.isIntersecting&&throttle){
+            throttle=false
+            observer.disconnect()
+            if(!searchState){
+                random_fetchAPI(pNum,28)
+            }else{
+                search_fetchAPI(pNum,28,$('.dropBtn').text().toLowerCase(),$('#searchWord').val())
+            }
         }
     })
 }
@@ -20,10 +27,19 @@ const callback = (entries,observer)=>{
 
 // })
 var color = ''
-$('.drop-Content span').click(function (e) { 
-    var color= $(this).text()
-    $('.dropBtn').css('background-color', $(this).css('background-color'));
-    console.log(color)    
+$('.drop-Content span').click(function () {
+    if ($(this).text()!='None'){
+        color= $(this).text()
+        $('.dropBtn').text(color);
+        $('.dropBtn').css('background-color', $(this).css('background-color'));
+        $('.dropBtn').css('color', $(this).css('color'));
+        console.log(color)   
+    }else{
+        color = ''
+        $('.dropBtn').text('None');
+        $('.dropBtn').css('background-color', $(this).css('background-color'));
+        $('.dropBtn').css('color', $(this).css('color'));
+    }
 });
 const option= {
     root:null,
@@ -31,8 +47,17 @@ const option= {
     threshhold:0
 }
 const observer = new IntersectionObserver(callback,option)
+
 $(document).ready(function(){
     random_fetchAPI(pNum,28)
+    $('#searchButton').click(function(){
+        $('#imgList1').empty();
+        $('#imgList2').empty();
+        $('#imgList3').empty();
+        $('#imgList4').empty();
+        pNum=1
+        search_fetchAPI(pNum,28,$('.dropBtn').text().toLowerCase(),$('#searchWord').val())
+    })
   });
 
 function random_fetchAPI(pageNum=1,perPage){
@@ -40,37 +65,82 @@ function random_fetchAPI(pageNum=1,perPage){
     .then((res)=>res.json())
     .then((res)=>{
         console.log(res)
-        observer.disconnect()
-        for (let i = 0; i < perPageNum; i++) {
-            $('#imgList1').append('<div id="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
-            '"target="_blank"><img class="imgClass1" src="'+res[i].urls.regular+'">'+
-            '<span id="imgDesc">'+
-            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
-        }
-            observer.observe(document.getElementById("imgList1").lastElementChild)
-        for (let i = perPageNum; i < 2*perPageNum; i++) {
-            $('#imgList2').append('<div id="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
-            '"target="_blank"><img class="imgClass2" src="'+res[i].urls.regular+'">'+
-            '<span id="imgDesc">'+
-            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
-        }
-            observer.observe(document.getElementById("imgList2").lastElementChild)
-        for (let i = 2*perPageNum; i < 3*perPageNum; i++) {
-            $('#imgList3').append('<div class="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
-            '"target="_blank"><img class="imgClass3" src="'+res[i].urls.regular+'">'+
-            '<span id="imgDesc">'+
-            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
-        }
-        observer.observe(document.getElementById("imgList3").lastElementChild)
-        for (let i = 3*perPageNum; i < 4*perPageNum; i++) {
-            $('#imgList4').append('<div class="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
-            '"target="_blank"><img class="imgClass4" src="'+res[i].urls.regular+'">'+
-            '<span id="imgDesc">'+
-            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
-        }
-        observer.observe(document.getElementById("imgList4").lastElementChild)
-
-      pNum++
-      console.log(pNum)
+        after_Fetch(res)
+        console.log(pNum)
     })
+}
+var searchState=false
+function search_fetchAPI(pageNum=1,perPage,color='',keyword=''){
+    var query='&query='
+    var qColor = '&color='
+    if(keyword!=''){query += keyword}else{query = ''}
+    if(color!=''&&color!='none'&&color!='color'){qColor += color}else{qColor = ''}
+    if(color=='b & w'){qColor = '&color=black_and_white'}
+    if (query){
+        fetch('https://api.unsplash.com/search/photos?page='+pageNum+'&per_page='+perPage+
+        query+qColor+
+        '&'+ AccessKey)
+        .then((res)=>res.json())
+        .then((res)=>{
+            console.log(res)
+            console.log('https://api.unsplash.com/search/photos?&page='+pageNum+'&per_page='+perPage+
+            query+qColor+
+            '&'+ AccessKey)
+            if(res.total==0){
+                $('main').append('<h1 style="margin:0 auto 10% auto;width:30%;color:gray;text-align:center;font-size:5em">No Result</h1>')
+                return;
+            }
+            after_Fetch(res.results)
+            console.log(pNum)
+            searchState=true
+            
+        })
+        // .catch(err => {console.log(err)})
+    }else{
+        random_fetchAPI(pNum,28)
+    }
+}
+
+function after_Fetch(res){
+    for (let i = 0; i < perPageNum; i++) {
+        if(typeof res[i]!='undefined'){
+            $('#imgList1').append('<div id="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
+            '"target="_blank"><img class="imgClass1" src="'+res[i].urls.regular+'"><span id="imgDesc">'+
+            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
+        }
+    }
+    for (let i = perPageNum; i < 2*perPageNum; i++) {
+        if(typeof res[i]!='undefined'){
+            $('#imgList2').append('<div id="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
+            '"target="_blank"><img class="imgClass2" src="'+res[i].urls.regular+'"><span id="imgDesc">'+
+            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
+        }
+    }
+    for (let i = 2*perPageNum; i < 3*perPageNum; i++) {
+        if(typeof res[i]!='undefined'){
+            $('#imgList3').append('<div class="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
+            '"target="_blank"><img class="imgClass3" src="'+res[i].urls.regular+'"><span id="imgDesc">'+
+            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
+        }
+        
+    }
+    for (let i = 3*perPageNum; i < 4*perPageNum; i++) {
+        if(typeof res[i]!='undefined'){
+            $('#imgList4').append('<div class="imgItem'+(i+1)+'"><a href="'+res[i].links.html+
+            '"target="_blank"><img class="imgClass4" src="'+res[i].urls.regular+'"><span id="imgDesc">'+
+            setUpperCase(res[i].alt_description) +'<br> by <br>'+res[i].user.name +'</span>'+'</a></div>');
+        }
+    }
+    observeImg()
+    setTimeout(() => {
+        throttle=true
+    }, 500);
+    pNum++
+}
+
+function observeImg(){
+    observer.observe(document.getElementById("imgList1").lastElementChild)
+    observer.observe(document.getElementById("imgList2").lastElementChild)
+    observer.observe(document.getElementById("imgList3").lastElementChild)
+    observer.observe(document.getElementById("imgList4").lastElementChild)
 }
